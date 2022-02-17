4db8298c89880116e6f0ffde7b557c3834e01c98
go0n87 <98361866+go0n87@users.noreply.github.com>
2022-02-18 00:55:48 +0300

    0:55 18.02.2022

---
+++ Assets/Scripts/LoggingEvenNumbers.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoggingEvenNumbers : MonoBehaviour
+{    
+    [SerializeField] private InputField sizeArray;
+
+    private int numberElements;    
+
+    private int[] nums = new int[0];
+
+    public void onClick()
+    {
+
+        if (sizeArray.text != "")
+        {
+            numberElements = Convert.ToInt32(sizeArray.text);
+
+            Array.Resize(ref nums, numberElements);
+
+            for (int i = 0; i < numberElements; i++)
+            {
+                nums[i] = i;
+            }
+
+            foreach (int indexValues in nums)
+            {
+                if (indexValues % 2 == 0)
+                {
+                    Debug.Log(indexValues);
+                }
+            }
+
+            Array.Resize(ref nums, 0);
+        }
+        else 
+        {
+            Debug.Log("Не задан размер массива!");
+        }
+    }
+
+}
